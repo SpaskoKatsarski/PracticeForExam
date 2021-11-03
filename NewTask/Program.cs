f065deabeae66484c7fe3948af45ef757567b9ba
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-11-03 17:17:05 +0200

    Practicing (Small update)

---
+++ NewTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = string.Empty;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    str = "even";
+                }
+                else
+                {
+                    str = "odd";
+                }
+                Console.WriteLine($"The current text is in {str} position.", str);
+            }
+        }
+    }
+}
